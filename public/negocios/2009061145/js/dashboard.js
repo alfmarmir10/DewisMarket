b87710a1d8eeb6caad30789d454ac1a0df9a1491
Alfonso Martínez <alfmar100391@icloud.com>
2020-11-04 13:06:25 -0600

    Avance en nuevo Dashboard para Negocios

---
+++ public/negocios/2009061145/js/dashboard.js
@@ -0,0 +1,34 @@
+function loadDashboardCharts(){
+  var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+  var lineChartData = {
+    labels : ["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"],
+    datasets : [
+      {
+        label: "Semana Pasada",
+        fillColor : "rgba(220,220,220,0.2)",
+        strokeColor : "rgba(220,220,220,1)",
+        pointColor : "rgba(220,220,220,1)",
+        pointStrokeColor : "#fff",
+        pointHighlightFill : "#fff",
+        pointHighlightStroke : "rgba(220,220,220,1)",
+        data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+      },
+      {
+        label: "Semana Actual",
+        fillColor : "rgba(151,187,205,0.2)",
+        strokeColor : "rgba(151,187,205,1)",
+        pointColor : "rgba(151,187,205,1)",
+        pointStrokeColor : "#fff",
+        pointHighlightFill : "#fff",
+        pointHighlightStroke : "rgba(151,187,205,1)",
+        data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+      }
+    ]
+  }
+  var ctx = document.getElementById("canvas").getContext("2d");
+  window.myLine = new Chart(ctx).Line(lineChartData, {
+      responsive: true
+  })
+}
+
+
