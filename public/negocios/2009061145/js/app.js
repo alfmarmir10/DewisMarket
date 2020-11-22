0cc0b5f72ce9415a6f7ecd1278b4dd93c36e59e6
Alfonso Martínez Mireles <aceone@MacBook-Pro-de-Alfonso.local>
2020-11-22 13:37:54 -0600

    Se eliminan archivos de la versión anterior. _respaldo.html

--- public/negocios/2009061145/js/app.js
+++ public/negocios/2009061145/js/app.js
@@ -2588,6 +2588,57 @@ function renderGraphsProducto(documentos, unidades){
     chart.render().then(banderaX = true);
  }
 
+
+ function loadGraphsCat1(unidadesBtn){
+    var options = {
+        series: [44, 55, 13, 43, 22],
+        chart: {
+        width: 380,
+        type: 'pie',
+      },
+      labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }]
+    };
+
+      var chartCat1 = new ApexCharts(document.querySelector("#chartCat1"), options);
+      chartCat1.render();
+}
+
+function loadGraphsCat2(unidadesBtn){
+    var options = {
+        series: [44, 55, 13, 43, 22],
+        chart: {
+        width: 380,
+        type: 'pie',
+      },
+      labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }]
+    };
+
+      var chartCat2 = new ApexCharts(document.querySelector("#chartCat2"), options);
+      chartCat2.render();
+}
+
 function loadIndexGraphs(unidadesBtn){
     var idNegocio = getCookie("idNegocio");
     var fecha7 = new Date();
@@ -2762,96 +2813,79 @@ function renderIndexGraphs(documentos, unidades){
 
         chart.render();
 
-        var options = {
-        series: [{
-        name: 'TEAM A',
-        type: 'column',
-        data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30]
-        }, {
-        name: 'TEAM B',
-        type: 'area',
-        data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43]
-        }, {
-        name: 'TEAM C',
-        type: 'line',
-        data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39]
-        }],
-        chart: {
-        height: 350,
-        type: 'line',
-        stacked: false,
-        },
-        stroke: {
-        width: [0, 2, 5],
-        curve: 'smooth'
-        },
-        plotOptions: {
-        bar: {
-            columnWidth: '50%'
-        }
-        },
+        // var options = {
+        // series: [{
+        // name: 'TEAM A',
+        // type: 'column',
+        // data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30]
+        // }, {
+        // name: 'TEAM B',
+        // type: 'area',
+        // data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43]
+        // }, {
+        // name: 'TEAM C',
+        // type: 'line',
+        // data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39]
+        // }],
+        // chart: {
+        // height: 350,
+        // type: 'line',
+        // stacked: false,
+        // },
+        // stroke: {
+        // width: [0, 2, 5],
+        // curve: 'smooth'
+        // },
+        // plotOptions: {
+        // bar: {
+        //     columnWidth: '50%'
+        // }
+        // },
         
-        fill: {
-        opacity: [0.85, 0.25, 1],
-        gradient: {
-            inverseColors: false,
-            shade: 'light',
-            type: "vertical",
-            opacityFrom: 0.85,
-            opacityTo: 0.55,
-            stops: [0, 100, 100, 100]
-        }
-        },
-        labels: ['01/01/2003', '02/01/2003', '03/01/2003', '04/01/2003', '05/01/2003', '06/01/2003', '07/01/2003',
-        '08/01/2003', '09/01/2003', '10/01/2003', '11/01/2003'
-        ],
-        markers: {
-        size: 0
-        },
-        xaxis: {
-        type: 'datetime'
-        },
-        yaxis: {
-        title: {
-            text: 'Points',
-        },
-        min: 0
-        },
-        tooltip: {
-        shared: true,
-        intersect: false,
-        y: {
-            formatter: function (y) {
-            if (typeof y !== "undefined") {
-                return y.toFixed(0) + " points";
-            }
-            return y;
+        // fill: {
+        // opacity: [0.85, 0.25, 1],
+        // gradient: {
+        //     inverseColors: false,
+        //     shade: 'light',
+        //     type: "vertical",
+        //     opacityFrom: 0.85,
+        //     opacityTo: 0.55,
+        //     stops: [0, 100, 100, 100]
+        // }
+        // },
+        // labels: ['01/01/2003', '02/01/2003', '03/01/2003', '04/01/2003', '05/01/2003', '06/01/2003', '07/01/2003',
+        // '08/01/2003', '09/01/2003', '10/01/2003', '11/01/2003'
+        // ],
+        // markers: {
+        // size: 0
+        // },
+        // xaxis: {
+        // type: 'datetime'
+        // },
+        // yaxis: {
+        // title: {
+        //     text: 'Points',
+        // },
+        // min: 0
+        // },
+        // tooltip: {
+        // shared: true,
+        // intersect: false,
+        // y: {
+        //     formatter: function (y) {
+        //     if (typeof y !== "undefined") {
+        //         return y.toFixed(0) + " points";
+        //     }
+        //     return y;
         
-            }
-        }
-        }
-        };
+        //     }
+        // }
+        // }
+        // };
 
-        var chart = new ApexCharts(document.querySelector("#chart2"), options);
-        chart.render();
-        /* $("#graphVentaTotal").sparkline([getNum(documentos[0][fecha1String]), getNum(documentos[0][fecha2String]),
-        getNum(documentos[0][fecha3String]), getNum(documentos[0][fecha4String]), getNum(documentos[0][fecha5String]), getNum(documentos[0][fecha6String]), getNum(documentos[0][fecha7String])], {
-            type: 'line',
-            width: '99.5%',
-            height: '100',
-            lineColor: '#5969ff',
-            fillColor: '#dbdeff',
-            lineWidth: 2,
-            spotColor: undefined,
-            minSpotColor: undefined,
-            maxSpotColor: undefined,
-            highlightSpotColor: 'deeppink',
-            highlightLineColor: undefined,
-            resize: true
-        });
-
-        document.getElementById('totalVentaTotal').innerHTML = "$"+(getNum(documentos[0][fecha1String])+getNum(documentos[0][fecha2String])+getNum(documentos[0][fecha3String])+getNum(documentos[0][fecha4String])+
-        getNum(documentos[0][fecha5String])+getNum(documentos[0][fecha6String])+getNum(documentos[0][fecha7String])).toString(); */
+        // var chart = new ApexCharts(document.querySelector("#chart2"), options);
+        // chart.render();
+        
  }
 
 
